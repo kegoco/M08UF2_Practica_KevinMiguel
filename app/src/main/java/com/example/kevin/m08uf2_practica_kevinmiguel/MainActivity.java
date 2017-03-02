7db6633b828153c40ff6bbcd399c2303492e3394
mihema <maikel007@hotmail.es>
2017-03-02 14:19:16 +0100

    Diseño botones

--- app/src/main/java/com/example/kevin/m08uf2_practica_kevinmiguel/MainActivity.java
+++ app/src/main/java/com/example/kevin/m08uf2_practica_kevinmiguel/MainActivity.java
@@ -2,14 +2,24 @@ package com.example.kevin.m08uf2_practica_kevinmiguel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn01;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Kevin es un `paquete y siempre me hace trabajar como un negro
-        // Eso es mentira!!!!!!!!
+
     }
+    public void onClick(View v){
+        System.out.println("funciona");
+
+
+    }
+
 }
