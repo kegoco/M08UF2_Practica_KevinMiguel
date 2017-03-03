ad6795284300faa73d7375ce94eec9ab1db91320
mihema <maikel007@hotmail.es>
2017-03-03 10:10:29 +0100

    Diseño botones

--- app/src/main/java/com/example/kevin/m08uf2_practica_kevinmiguel/MainActivity.java
+++ app/src/main/java/com/example/kevin/m08uf2_practica_kevinmiguel/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn01;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
